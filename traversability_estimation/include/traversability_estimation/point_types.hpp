f5924811c24bbca2777cad2e7cabd133d620b683
Maik Knof <knofm@hs-weingarten.de>
2025-03-01 12:06:13 +0100

    change variables to align more with the paper

--- traversability_estimation/include/traversability_estimation/point_types.hpp
+++ traversability_estimation/include/traversability_estimation/point_types.hpp
@@ -8,9 +8,9 @@ struct PointXYZICluster
 {
     PCL_ADD_POINT4D;  // Adds x, y, z, data[4]
     PCL_ADD_NORMAL4D; // Adds normal_x, normal_y, normal_z, data_n[4]
-    float curvature;
-    float intensity;
-    uint32_t cluster_id;
+    float intensity; // Typically part of LiDAR output, not used atm
+    float curvature; // Value between 0 and 1 (c_i^{(c)} in the paper)
+    uint32_t cluster_id; // Cluster ID
     EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 } EIGEN_ALIGN16;
 
@@ -23,8 +23,8 @@ POINT_CLOUD_REGISTER_POINT_STRUCT(PointXYZICluster, //
                                   (float, normal_x, normal_x)        //
                                   (float, normal_y, normal_y)        //
                                   (float, normal_z, normal_z)        //
-                                  (float, curvature, curvature)      //
                                   (float, intensity, intensity)      //
+                                  (float, curvature, curvature)      //
                                   (uint32_t, cluster_id, cluster_id) //
 )
 
@@ -32,14 +32,15 @@ struct TraversablePoint
 {
     PCL_ADD_POINT4D;  // Adds x, y, z, data[4]
     PCL_ADD_NORMAL4D; // Adds normal_x, normal_y, normal_z, data_n[4]
-    float slope;
-    float curvature;
-    float intensity;
-    uint32_t cluster_id;
-    float inflation_cost;
-    float slope_cost;
-    float curvature_cost;
-    float final_cost;
+    float intensity; // Typically part of LiDAR output, not used atm
+    float curvature; // Value between 0 and 1 (c_i^{(c)} in the paper)
+    float slope; // Value between 0 and 1 (c_i^{(s)} in the paper)
+    uint32_t cluster_id; // Cluster ID
+    float inflation; // Value between 0 and 1 (c_i^{(i)} in the paper)
+    float curvature_cost; // curvature * w^{(c)}
+    float slope_cost; // slope * w^{(s)}
+    float inflation_cost; // inflation * w^{(i)}
+    float final_cost; // curvature_cost + slope_cost + inflation_cost
     EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 } EIGEN_ALIGN16;
 
@@ -52,15 +53,16 @@ POINT_CLOUD_REGISTER_POINT_STRUCT(TraversablePoint, //
                                   (float, normal_x, normal_x)   //
                                   (float, normal_y, normal_y)   //
                                   (float, normal_z, normal_z)   //
+                                  // Padding from PCL_ADD_NORMAL4D
+                                  (float, data_n[4], data_n)    //
+                                  (float, intensity, intensity) //
                                   (float, slope, slope)         //
                                   (float, curvature, curvature) //
-                                  // Padding from PCL_ADD_NORMAL4D
-                                  (float, data_n[4], data_n)              //
-                                  (float, intensity, intensity)           //
                                   (uint32_t, cluster_id, cluster_id)      //
-                                  (float, inflation_cost, inflation_cost) //
-                                  (float, slope_cost, slope_cost)         //
+                                  (float, inflation, inflation)           //
                                   (float, curvature_cost, curvature_cost) //
+                                  (float, slope_cost, slope_cost)         //
+                                  (float, inflation_cost, inflation_cost) //
                                   (float, final_cost, final_cost)         //
 )
 
